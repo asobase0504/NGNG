6cd8814be1f2444525aa04dbd1c2befb444046d9
asobase0504 <92970570+asobase0504@users.noreply.github.com>
2023-10-24 13:16:40 +0900

    競合で起こった不具合の修正

--- project/src/utility.cpp
+++ project/src/utility.cpp
@@ -54,14 +54,6 @@ bool Homing(D3DXVECTOR3 *pPosOut, const D3DXVECTOR3 &posNow, const D3DXVECTOR3 &
 }
 
 //--------------------------------------------------------------
-// ê¨å˜ämó¶
-//--------------------------------------------------------------
-bool IsSuccessRate(float inRate)
-{
-	return ((rand() / (float)RAND_MAX)) < inRate;
-}
-
-//--------------------------------------------------------------
 // è¨êîì_ÇÃÉâÉìÉ_ÉÄ
 //--------------------------------------------------------------
 float FloatRandom(float fMax, float fMin)
