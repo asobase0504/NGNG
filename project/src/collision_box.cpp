0f495999ea0341f6ac108f9d274f196811e1759f
asobase0504 <92970570+asobase0504@users.noreply.github.com>
2023-11-07 16:57:15 +0900

    当たり判定を見た目に合わせる

--- project/src/collision_box.cpp
+++ project/src/collision_box.cpp
@@ -42,23 +42,23 @@ void CCollisionBox::Update()
 	D3DXVECTOR3 size = GetSize();
 	D3DXVECTOR3 rot = GetRot();
 
-	float left = -size.x * 0.5f;	// x1
-	float right = size.x * 0.5f;	// x2
-	float back = size.z * 0.5f;		// z1
-	float front = -size.z * 0.5f;	// z2
+	float left = -size.x;	// x1
+	float right = size.x;	// x2
+	float back = size.z;		// z1
+	float front = -size.z;	// z2
 	float top = size.y;		// y2
 	float bot = -size.y;		// y2
 
 	// ‚S‚Â‚Ì’¸“_
 	D3DXVECTOR3 posLine[8];
-	posLine[0] = D3DXVECTOR3(left - 10.0f, top, back + 10.0f);
-	posLine[1] = D3DXVECTOR3(right + 10.0f, top, back + 10.0f);
-	posLine[2] = D3DXVECTOR3(right + 10.0f, top, front - 10.0f);
-	posLine[3] = D3DXVECTOR3(left - 10.0f, top, front - 10.0f);
-	posLine[4] = D3DXVECTOR3(left - 10.0f, bot, back + 10.0f);
-	posLine[5] = D3DXVECTOR3(right + 10.0f, bot, back + 10.0f);
-	posLine[6] = D3DXVECTOR3(right + 10.0f, bot, front - 10.0f);
-	posLine[7] = D3DXVECTOR3(left - 10.0f, bot, front - 10.0f);
+	posLine[0] = D3DXVECTOR3(left, top, back);
+	posLine[1] = D3DXVECTOR3(right, top, back);
+	posLine[2] = D3DXVECTOR3(right, top, front);
+	posLine[3] = D3DXVECTOR3(left, top, front);
+	posLine[4] = D3DXVECTOR3(left, bot, back);
+	posLine[5] = D3DXVECTOR3(right, bot, back);
+	posLine[6] = D3DXVECTOR3(right, bot, front);
+	posLine[7] = D3DXVECTOR3(left, bot, front);
 
 	m_line[0]->SetLine(pos, rot, posLine[0], posLine[1], D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f));
 	m_line[1]->SetLine(pos, rot, posLine[1], posLine[2], D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f));
@@ -87,7 +87,7 @@ CCollisionBox* CCollisionBox::Create(const D3DXVECTOR3& pos, const D3DXVECTOR3& 
 	collision->SetPos(pos);
 	collision->SetRot(rot);
 	collision->SetMtxWorld(mtx);
-	collision->m_size = size;
+	collision->SetSize(size);
 
 	return collision;
 }
