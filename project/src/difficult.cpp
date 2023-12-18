b1268f2b04b33e8cc02c5862611d9df060746d61
GoriGoriGorilla <211090@st.yoshida-g.ac.jp>
2023-12-18 13:35:59 +0900

    難易度の実装(敵に適応されていない)

--- project/src/difficult.cpp
+++ project/src/difficult.cpp
@@ -9,6 +9,7 @@
 // include
 //==============================================================
 #include "difficult.h"
+#include "object2d.h"
 
 //--------------------------------------------------------------
 // コンストラクタ
@@ -34,16 +35,14 @@ CDifficult::~CDifficult()
 //--------------------------------------------------------------
 HRESULT CDifficult::Init()
 {
-	CObject::Init();
+	m_bar = CObject2d::Create(CTaskGroup::EPriority::LEVEL_2D_UI);
+	m_bar->SetAnchor(CObject2d::EAnchor::ANCHOR_LEFT);
+	m_bar->SetSize(D3DXVECTOR3(200.0f, 30.0f, 0.0f));
+	m_bar->SetPos(D3DXVECTOR3(SCREEN_WIDTH - 500.0f, 70.0f, 0.0f));
+	m_bar->SetColor(D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
+	m_bar->SetTexture("DIFFICULT");
+	m_bar->SetTex(PositionVec4(0.0f,0.2f,0.0f,1.0f));
 
-	return S_OK;
-}
-
-//--------------------------------------------------------------
-// 初期化(オーバーロード)
-//--------------------------------------------------------------
-HRESULT CDifficult::Init(const D3DXVECTOR3& inPos, const D3DXVECTOR3& inRot, const D3DXVECTOR3& inSize)
-{
 	CObject::Init();
 
 	return S_OK;
@@ -67,12 +66,28 @@ void CDifficult::Update()
 		m_time++;
 	}
 
-	if (m_time >= 300)
+	float size = 0.2f;
+	float gage1 = 0.0f;
+
+	if (m_time >= 1000)
 	{
 		m_gameLevel++;
 		m_enemyLevel = m_gameLevel * 10;
 		m_time = 0;
 	}
+
+	if (m_gameLevel >= 3.0f)
+	{
+		gage1 = 3.0f * size;
+	}
+	else
+	{
+		gage1 = m_gameLevel * size;
+	}
+
+	float gage2 = m_time * 0.0002f;
+
+	m_bar->SetTex(PositionVec4(gage1 + gage2, size + gage1 + gage2, 0.0f, 1.0f));
 
 	CObject::Update();
 }
@@ -95,7 +110,7 @@ CDifficult* CDifficult::Create(const D3DXVECTOR3& inPos, const D3DXVECTOR3& inRo
 
 	if (pDifficult != nullptr)
 	{
-		pDifficult->Init(inPos, inRot, inSize);
+		pDifficult->Init();
 		pDifficult->SetPos(inPos);
 		pDifficult->SetRot(inRot);
 		pDifficult->SetSize(inSize);
