f7d6c196a7649023208023d57808f1802fbc70d6
asobase0504 <92970570+asobase0504@users.noreply.github.com>
2023-12-20 17:16:30 +0900

    少し、軽く

--- project/src/objectX.cpp
+++ project/src/objectX.cpp
@@ -209,7 +209,7 @@ void CObjectX::Draw()
 	//マテリアルデータのポインタを取得する
 	D3DXMATERIAL* pMat = (D3DXMATERIAL*)m_buffMat->GetBufferPointer();
 
-	LPDIRECT3DTEXTURE9 texture = CTexture::GetInstance()->GetTexture("TOON");
+	static LPDIRECT3DTEXTURE9 texture = CTexture::GetInstance()->GetTexture("TOON");
 
 	for (int nCntMat = 0; nCntMat < (int)m_numMat; nCntMat++)
 	{
